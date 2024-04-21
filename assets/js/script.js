e6560507606a36c87d49fffdf11f3b8e419edece
Isaac <42228727+isaacchook@users.noreply.github.com>
2024-04-21 15:17:59 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,28 @@
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
+customElements.define('flag-emoji', class extends HTMLElement {
 
+  /**
+   * Create a new instance
+   */
+  constructor () {
+      super();
+      this.textContent = this.getFlagEmoji(this.getAttribute('code'));
+  }
+
+  /**
+   * Get the flag emoji for the country
+   * @link https://dev.to/jorik/country-code-to-flag-emoji-a21
+   * @param  {String} countryCode The country code
+   * @return {String}             The flag emoji
+   */
+  getFlagEmoji (countryCode) {
+      let codePoints = countryCode.toUpperCase().split('').map(char =>  127397 + char.charCodeAt());
+      return String.fromCodePoint(...codePoints);
+  }
+
+});
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
